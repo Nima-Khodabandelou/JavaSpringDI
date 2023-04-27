131acec6db34f876799f53b35c9a35ef258983eb
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-27 16:49:49 +0330

    Update SetterInjectedGreetingService.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/services/SetterInjectedGreetingService.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/services/SetterInjectedGreetingService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SetterInjectedGreetingService implements GreetingService {
+
     @Override
     public String sayGreeting() {
         return "Hello! - Setter";
