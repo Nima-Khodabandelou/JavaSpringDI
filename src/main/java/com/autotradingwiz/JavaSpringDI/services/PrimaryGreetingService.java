4c1c16033eabe7167a05f8c0a397889ac2631f26
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-27 16:49:20 +0330

    Update PrimaryGreetingService.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/services/PrimaryGreetingService.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/services/PrimaryGreetingService.java
@@ -2,13 +2,16 @@ package com.autotradingwiz.JavaSpringDI.services;
 
 
 import org.springframework.context.annotation.Primary;
+
 import org.springframework.stereotype.Service;
 
 @Primary
 @Service
 public class PrimaryGreetingService implements GreetingService {
+
     @Override
     public String sayGreeting() {
         return "Hi! - Using primary bean";
     }
+
 }
