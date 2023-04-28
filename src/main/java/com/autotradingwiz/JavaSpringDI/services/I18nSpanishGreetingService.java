6fce4f6a2bd37b3b13cd2ba38278c57f9e55000f
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-28 07:39:15 +0330

    Update I18nSpanishGreetingService.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/services/I18nSpanishGreetingService.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/services/I18nSpanishGreetingService.java
@@ -3,13 +3,13 @@ package com.autotradingwiz.JavaSpringDI.services;
 import org.springframework.context.annotation.Profile;
 
 import org.springframework.stereotype.Service;
-
+// internationalization
 @Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hi folks in Spain!";
+        return "Hi Spanish!";
     }
 }
