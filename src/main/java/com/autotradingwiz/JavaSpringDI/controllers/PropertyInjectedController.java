ea6f0af6007855abf3846951a34b0df174a07b83
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-04 12:56:10 +0330

    Update PropertyInjectedController.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/controllers/PropertyInjectedController.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/controllers/PropertyInjectedController.java
@@ -1,8 +1,11 @@
 package com.autotradingwiz.JavaSpringDI.controllers;
 
 import com.autotradingwiz.JavaSpringDI.services.GreetingService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
