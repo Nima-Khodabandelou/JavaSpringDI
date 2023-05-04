38c84f4364281b1b47d957b61f51f3180957e6e2
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-04 14:21:45 +0330

    Update SetterInjectedController.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/controllers/SetterInjectedController.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/controllers/SetterInjectedController.java
@@ -1,8 +1,11 @@
 package com.autotradingwiz.JavaSpringDI.controllers;
 
 import com.autotradingwiz.JavaSpringDI.services.GreetingService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,6 +15,7 @@ public class SetterInjectedController {
     @Qualifier("setterInjectedGreetingService")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
+
         this.greetingService = greetingService;
     }
 
