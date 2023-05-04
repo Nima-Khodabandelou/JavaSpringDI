2873347d051f9ba565c72f8c9eb69ff49264fcbe
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-04 14:22:22 +0330

    Update ConstructorInjectedController.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/controllers/ConstructorInjectedController.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/controllers/ConstructorInjectedController.java
@@ -2,7 +2,9 @@ package com.autotradingwiz.JavaSpringDI.controllers;
 
 
 import com.autotradingwiz.JavaSpringDI.services.GreetingService;
+
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,6 +12,7 @@ public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
     public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+
         this.greetingService = greetingService;
     }
 
