98e058517872203c8dfa4b4a432e2998985fa1ed
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-04 14:22:06 +0330

    Update I18nController.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/controllers/I18nController.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/controllers/I18nController.java
@@ -1,7 +1,9 @@
 package com.autotradingwiz.JavaSpringDI.controllers;
 
 import com.autotradingwiz.JavaSpringDI.services.GreetingService;
+
 import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,6 +11,7 @@ public class I18nController {
     private final GreetingService greetingService;
 
     public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+
         this.greetingService = greetingService;
     }
 
