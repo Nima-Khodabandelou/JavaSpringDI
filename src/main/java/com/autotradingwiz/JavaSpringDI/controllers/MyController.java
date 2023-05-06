001b4aee566f8280503e850808b8ccd26025c025
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-06 10:52:32 +0330

    Update MyController.java

--- src/main/java/com/autotradingwiz/JavaSpringDI/controllers/MyController.java
+++ src/main/java/com/autotradingwiz/JavaSpringDI/controllers/MyController.java
@@ -1,11 +1,11 @@
 package com.autotradingwiz.JavaSpringDI.controllers;
 
 import com.autotradingwiz.JavaSpringDI.services.GreetingService;
-
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    
     private final GreetingService greetingService;
 
     public MyController(GreetingService greetingService) {
